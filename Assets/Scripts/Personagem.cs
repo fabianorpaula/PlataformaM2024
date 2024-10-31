c6612b4ec2f17d884ea5fae41dd5de5b18d49800
fabianorpaula <fabiano.rpaula@gmail.com>
2024-10-31 08:08:53 -0300

    Personagem Correndo

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -6,15 +6,38 @@ public class Personagem : MonoBehaviour
 {
 
     public Rigidbody2D Corpo;
+    public Animator Animador;
     void Start()
     {
         Corpo = GetComponent<Rigidbody2D>();
+        Animador = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float velH = Input.GetAxis("Horizontal")*4;
+        Mover();
+    }
+
+    void Mover()
+    {
+        float velH = Input.GetAxis("Horizontal") * 4;
+        if(velH < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            Animador.SetBool("Andar", true);
+        }
+        if (velH > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            Animador.SetBool("Andar", true);
+        }
+        if(velH == 0)
+        {
+            Animador.SetBool("Andar", false);
+        }
+
+
         float velY = Corpo.velocity.y;
-        Corpo.velocity = new Vector2 (velH, velY);
+        Corpo.velocity = new Vector2(velH, velY);
     }
 }
